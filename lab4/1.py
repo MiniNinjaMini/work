245fb95df1b28835cecbfd54e5b55e722bee8d4e
MiniNinjaMini <sanjbob00@mail.ru>
2025-02-10 14:37:25 +0500

    lab4v2

--- lab4/1.py
+++
@@ -1,8 +0,0 @@
-import math
-
-degree = float(input("Input degree: "))
-
-radian = degree * (math.pi / 180)
-
-# Display result rounded to 6 decimal places
-print("Output radian:", round(radian, 6))
