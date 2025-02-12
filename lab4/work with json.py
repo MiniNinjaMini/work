12f791f3583fd1896f581a3436e05b2c98b6d806
MiniNinjaMini <sanjbob00@mail.ru>
2025-02-12 19:03:39 +0500

    lab4v3

---
+++ lab4/work with json.py
@@ -0,0 +1,23 @@
+import json
+from pathlib import Path
+
+# Load the JSON file
+file_path = Path("sample-data.json")
+with file_path.open("r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Extract interface details
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':<50} {'Description':<15} {'Speed':<10} {'MTU':<5} ")
+print(f"{'-'*50}{' '}{'-'*15}{' '}{'-'*10}{' '}{'-'*5}")
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+    dn = attributes["dn"]
+    descr = attributes.get("descr", "N/A")
+    speed = attributes.get("speed", "N/A")
+    mtu = attributes.get("mtu", "N/A")
+   
+    
+    print(f"{dn:<50} {descr:<15} {speed:<10} {mtu:<5} ")
